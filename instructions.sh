2a64c6dccb1f30d6a04df92a1ee68b28f2961446
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-04 10:57:04 +1000

    initial

---
+++ instructions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+protoc -I proto proto/logbookpb/logbook.proto --go_out=./gen/go --go_opt=paths=source_relative \
+       --go-grpc_out=./gen/go --go-grpc_opt=paths=source_relative \
+       --grpc-gateway_out=./gen/go --grpc-gateway_opt=paths=source_relative
+# key -I: first param is the directory where the proto files are, second param is the proto file
+# key --go_out=./gen/go: output directory for generated files
+# key --go_opt=paths=source_relative: generated files will use the same package level
+# key --go-grpc_out=./gen/go: output directory for generated grpc files
+# key --go-grpc_opt=paths=source_relative: generated grpc files will use the same package level
+
+# This project needs to be shared into GitHub or other
